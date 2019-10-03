e3d9c49e6d963bd799039f5db991f40a1e223240
pranalikisannamdas <pranalinamdas96@gmail.com>
2019-10-03 15:18:43 +0530

    [Pranali|Satyajit] Add. Can not park two objects in parking lot of capaity one

--- src/main/java/com/thoughtworks/ParkingLot.java
+++ src/main/java/com/thoughtworks/ParkingLot.java
@@ -2,12 +2,18 @@ package com.thoughtworks;
 
 public class ParkingLot {
     private final int capacity;
+    private int spaceAvailable;
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        spaceAvailable = capacity;
     }
 
-    public boolean park(Object o) {
-        return true;
+    public boolean park(Object object) {
+        if (spaceAvailable > 0) {
+            spaceAvailable--;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/thoughtworks/ParkingLotTest.java
+++ src/test/java/com/thoughtworks/ParkingLotTest.java
@@ -2,14 +2,24 @@ package com.thoughtworks;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParkingLotTest {
 
     @Test
-    void givenParkingLotOfCapacityOne_whenParkOneObject_thenShouldBeAbleToPark(){
+    void givenParkingLotOfCapacityOne_whenParkOneObject_thenShouldBeAbleToPark() {
         ParkingLot parkingLot = new ParkingLot(1);
 
         assertTrue(parkingLot.park(new Object()));
+    }
+
+    @Test
+    void givenParkingLotCapacityOne_whenParkTwoObject_thenShouldNotBeAbleToParkSecondObject(){
+        ParkingLot parkingLot = new ParkingLot(1);
+
+        assertTrue(parkingLot.park(new Object()));
+        assertFalse(parkingLot.park(new Object()));
+
     }
 }
